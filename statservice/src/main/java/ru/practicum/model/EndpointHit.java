58480d6782f7af3aea3602bfe892e71633cd88ea
Stas <stas.vk88@yandex.ru>
2023-02-13 21:54:17 +0200

    Version 1.0 Patched

--- statservice/src/main/java/ru/practicum/model/EndpointHit.java
+++ statservice/src/main/java/ru/practicum/model/EndpointHit.java
@@ -1,19 +1,16 @@
 package ru.practicum.model;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 @Entity
 @Getter
 @Setter
 @ToString
+@EqualsAndHashCode
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Table(name = "endpoint_hit", schema = "public")
 public class EndpointHit {
@@ -32,17 +29,4 @@ public class EndpointHit {
 
     @Column(name = "time_stamp", nullable = false)
     LocalDateTime timestamp;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        EndpointHit that = (EndpointHit) o;
-        return Objects.equals(id, that.id) && Objects.equals(app, that.app) && Objects.equals(uri, that.uri) && Objects.equals(ip, that.ip) && Objects.equals(timestamp, that.timestamp);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, app, uri, ip, timestamp);
-    }
 }
--- statservice/src/main/java/ru/practicum/stats/StatsServiceImpl.java
+++ statservice/src/main/java/ru/practicum/stats/StatsServiceImpl.java
@@ -2,14 +2,17 @@ package ru.practicum.stats;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.dto.EndpointHitDto;
 import ru.practicum.mappers.StatsMapper;
-import ru.practicum.mappers.ViewStatsRowMapper;
 import ru.practicum.model.ViewStats;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -20,7 +23,7 @@ import java.util.List;
 @Slf4j
 public class StatsServiceImpl implements StatsService {
     private final StatsRepository repository;
-    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Override
     public List<ViewStats> getStats(String start, String end, List<String> uris, boolean unique) {
@@ -29,15 +32,18 @@ public class StatsServiceImpl implements StatsService {
             String sqlQuery;
             if (unique) {
                 sqlQuery = "SELECT app, uri, COUNT(DISTINCT ip) AS hits FROM endpoint_hit " +
-                        "WHERE uri = ? AND time_stamp > ? AND time_stamp < ? " +
+                        "WHERE uri = :uri AND time_stamp > :start AND time_stamp < :end " +
                         "GROUP BY app, uri ";
             } else {
                 sqlQuery = "SELECT app, uri, COUNT(ip) AS hits FROM endpoint_hit " +
-                        "WHERE uri = ? AND time_stamp > ? AND time_stamp < ? " +
+                        "WHERE uri = :uri AND time_stamp > :start AND time_stamp < :end " +
                         "GROUP BY app, uri ";
             }
-            List<ViewStats> viewStatsListByOneUri = jdbcTemplate.query(sqlQuery, new ViewStatsRowMapper(), uri,
-                    StatsMapper.stringToLocalDateTime(start), StatsMapper.stringToLocalDateTime(end));
+            SqlParameterSource namedParameters = new MapSqlParameterSource()
+                    .addValue("uri", uri)
+                    .addValue("start", StatsMapper.stringToLocalDateTime(start))
+                    .addValue("end", StatsMapper.stringToLocalDateTime(end));
+            List<ViewStats> viewStatsListByOneUri = namedParameterJdbcTemplate.query(sqlQuery, namedParameters, this::mapRow);
             viewStatsList.addAll(viewStatsListByOneUri);
         }
         viewStatsList.sort(Comparator.comparing(ViewStats::getHits).reversed());
@@ -48,5 +54,13 @@ public class StatsServiceImpl implements StatsService {
     @Override
     public EndpointHitDto saveHit(EndpointHitDto endpointHitDto) {
         return StatsMapper.toEndpointHitDto(repository.save(StatsMapper.toEndpointHit(endpointHitDto)));
+    }
+
+    private ViewStats mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return ViewStats.builder()
+                .app(rs.getString("app"))
+                .uri(rs.getString("uri"))
+                .hits(rs.getLong("hits"))
+                .build();
     }
 }
